5b3f63d347f63fbb8826f84e4d66b32dfbc6487e
Shiyamaladevi Pachaiappan <45041811+shyammeghna@users.noreply.github.com>
2020-02-12 16:35:08 +0100

    update contact.js
    update contact.js

--- components/contact.js
+++ components/contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
       <div className="contact-body">
         <Grid className="contact-grid">
           <Cell col={6}>
-            <h2>Paul Hanna</h2>
+            <h2>Shiyamaladevi</h2>
             <img
               src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
               alt="avatar"
@@ -24,28 +24,28 @@ class Contact extends Component {
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Shyam'}}>
                     <i className="fa fa-phone-square" aria-hidden="true"/>
                     (123) 456-7890
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Shyam'}}>
                     <i className="fa fa-fax" aria-hidden="true"/>
                     (123) 456-7890
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Shyam'}}>
                     <i className="fa fa-envelope" aria-hidden="true"/>
                     someone@example.com
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Shyam'}}>
                     <i className="fa fa-skype" aria-hidden="true"/>
                     MySkypeID
                   </ListItemContent>
